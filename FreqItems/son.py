fa4b2e0f7b7e7126242a5aaf7b75065a44f1445b
rpedsel <rpedsel@gmail.com>
2017-03-13 16:03:40 -0700

    main

--- FreqItems/son.py
+++ FreqItems/son.py
@@ -1,3 +1,9 @@
+
+'''
+spark-submit son.py [baskets file] [support ratio] [output file]
+
+'''
+
 from itertools import chain
 from itertools import ifilter, imap
 from collections import defaultdict
@@ -7,7 +13,6 @@ from pyspark import SparkContext
 import sys
 
 def superset(sets, m):
-    #print "--------------superset-------------------"
     if m == 2:
         return list(combinations(sets,m))
     sets = combinations(sets,m)
@@ -18,12 +23,10 @@ def superset(sets, m):
         count.update(chain(*sett))
         if len(count) == m and all(v == m-1 for v in count.values()):
             returnlist.append(tuple(sorted(count.keys())))
-    #print "--------------superset done-------------------"
     return returnlist
 
 
 def apriori(chunk,srate):
-    #print "----------------apriori-------------------"
     s = srate*len(chunk)
     returnlist = []
     C = defaultdict(int)
@@ -48,9 +51,6 @@ def apriori(chunk,srate):
             for t in tmp:
                 itemset = list(combinations(t,n-1))
                 if (n == 2 and all(it[0] in L for it in itemset)) or all(it in L for it in itemset): C[t]+=1
-            # for itemset in tmp:
-            #     if itemset in L:
-            #         C[itemset] += 1
         L = []
         for k,v in C.iteritems():
             if v >= s:
@@ -78,42 +78,34 @@ def basketcount(chunk,candidate):
     return list(count.iteritems())
 
 def basketcount_2(chunk, candidate):
-    #print "-------------------basketcount----------------------"
     C = defaultdict(int)
-#    C = Counter()
-    #print "generate...."
     all_set = ifilter(lambda x: x in candidate,chain(*[subsets(basket) for basket in chunk]))
-    #print "count"
     for sett in all_set:
         C[sett]+=1
-#    C.update(all_set)
-    #print "finish counting"
     return list(C.iteritems())
 
-sc = SparkContext(appName="inf553")
-srate = sys.argv[2]
-allbaskets = sc.textFile(sys.argv[1],3).map(lambda x: x.split(",")).map(lambda x: [int(i) for i in x]).cache()
+if __name__ == "__main__":
+    sc = SparkContext(appName="inf553")
+    srate = sys.argv[2]
+    allbaskets = sc.textFile(sys.argv[1],3).map(lambda x: x.split(",")).map(lambda x: [int(i) for i in x]).cache()
 
-def run_apriori(it):
-    return apriori(list(it),float(srate))
+    def run_apriori(it):
+        return apriori(list(it),float(srate))
 
-phase1 = allbaskets.mapPartitions(run_apriori).distinct().collect()
-#phase1 = allbaskets.mapPartitions(run_apriori).distinct()
-#print "--------------candidate----------------"
-#print phase1
-def run_basketcount(it):
-    return basketcount(list(it),phase1)
+    phase1 = allbaskets.mapPartitions(run_apriori).distinct().collect()
 
-f = open(sys.argv[3],'w')
+    def run_basketcount(it):
+        return basketcount(list(it),phase1)
 
-s = float(srate)*allbaskets.count()
-phase2 = allbaskets.mapPartitions(run_basketcount)
-#print "-------------result collect-------------"
-phase2 = phase2.reduceByKey(lambda x,y:x+y).filter(lambda x: x[1]>=s).map(lambda x: x[0]).collect()
-for itemset in phase2:
-    if type(itemset) == int:
-        f.write(str(itemset)+"\n")
-    else:
-        st = ",".join(map(str,itemset))
-        f.write(st+"\n")
-f.close()
+    f = open(sys.argv[3],'w')
+
+    s = float(srate)*allbaskets.count()
+    phase2 = allbaskets.mapPartitions(run_basketcount)
+    phase2 = phase2.reduceByKey(lambda x,y:x+y).filter(lambda x: x[1]>=s).map(lambda x: x[0]).collect()
+    for itemset in phase2:
+        if type(itemset) == int:
+            f.write(str(itemset)+"\n")
+        else:
+            st = ",".join(map(str,itemset))
+            f.write(st+"\n")
+    f.close()
